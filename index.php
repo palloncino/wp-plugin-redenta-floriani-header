677702932e996afad664a0aa68858dcd051839b1
palloncino <antonio.guiotto.dev@gmail.com>
2024-04-12 12:22:02 +0200

    progress

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ Author: Antonio Guiotto
 // Function to enqueue scripts and styles
 function header_strigno_scripts() {
     // Optionally, enqueue a CSS file if you have additional styles
-    // wp_enqueue_style('my-hello-world-css', plugins_url('/example_1942384.css', __FILE__));
+    wp_enqueue_style('my-hello-world-css', plugins_url('/hero-header-strigno.css', __FILE__));
     // Enqueue JavaScript file if necessary
     // wp_enqueue_script('my-hello-world-js', plugins_url('/example_1942384.js', __FILE__), array('jquery'), '1.0', true);
 }
@@ -19,7 +19,6 @@ add_action('wp_enqueue_scripts', 'header_strigno_scripts');
 function header_strigno_shortcode() {
     // Output the specified HTML
     return '<div class="hero-container hero-container-400">
-                <img class="hero-image" src="http://www.apspstrigno.it/w-images/resized/fademss/img55785cb1a2736.jpg" alt="APSP Pieve" />
                 <div class="hero-text-container hero-text-container-show">
                     <div class="text-wrapper">
                         <h1 class="main-title">APSP Strigno</h1>
